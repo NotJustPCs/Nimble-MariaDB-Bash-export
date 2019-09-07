019e476fff7b150e80f60f1bc2a98de646dc27ad
foshdafosh <peter@notjustpcs.co.uk>
2019-09-07 15:55:16 +0100

    Update nimbleimport.sh

--- nimbleimport.sh
+++ nimbleimport.sh
@@ -106,6 +106,7 @@ then
 										detail="${detail::-2}"
 										detailvalue="${detailvalue::-2}"
 									fi
+									detailvalue=${detailvalue//\"/}
 									insert_values="'$rec_nimb_cont_id','$sql_field','$modifier','$detailvalue','$label','$detail'"
 									insert_fields="cont_id,field,modifier,value,label,detail"
 									insert_statement="INSERT INTO $target_table ($insert_fields) VALUES ($insert_values)"
